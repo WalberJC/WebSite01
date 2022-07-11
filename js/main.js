23d0f5d8be8c50fe674338ba395bf834ebe9619f
Walber Gomes <103972585+WalberJC@users.noreply.github.com>
2022-07-11 20:38:54 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//$('.slider-principal').slick();
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 9000
+});
